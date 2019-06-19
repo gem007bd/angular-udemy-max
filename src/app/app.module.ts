e1be9add53cfcbf8362bcb804660430c37aba963
gem007bd <gem007bd@yahoo.com>
2019-06-19 16:10:31 +0200

    add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,24 @@
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
 import { AppRoutingModule } from './app-routing.module';
+// components
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './components/servers/servers.component';
+import { WarningComponent } from './components/warning/warning.component';
+import { HeaderComponent } from './components/header/header.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    WarningComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
