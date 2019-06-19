766d01cf9bec29fca09cc5c341936e36615b8b36
md Aminul hoque <gem007bd@yahoo.com>
2019-06-19 23:25:13 +0200

    fix all components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,13 @@
-
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { AppRoutingModule } from "./app-routing.module";
 // components
-import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './components/servers/servers.component';
-import { WarningComponent } from './components/warning/warning.component';
-import { HeaderComponent } from './components/header/header.component';
-
-
+import { AppComponent } from "./app.component";
+import { ServerComponent } from "./components/server/server.component";
+import { ServersComponent } from "./components/servers/servers.component";
+import { WarningComponent } from "./components/warning/warning.component";
+import { HeaderComponent } from "./components/header/header.component";
 
 @NgModule({
   declarations: [
@@ -20,12 +17,8 @@ import { HeaderComponent } from './components/header/header.component';
     WarningComponent,
     HeaderComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
