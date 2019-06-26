8a59b7f626803c809dc24cf96b138ec0216aea94
gem007bd <gem007bd@yahoo.com>
2019-06-26 13:57:56 +0200

    finish basic

--- src/app/components/servers/servers.component.ts
+++ src/app/components/servers/servers.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
-
+  secrectPassword = false;
+  log = [];
+  clickCount = 0;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onDetailClicked() {
+    this.secrectPassword = !this.secrectPassword;
+    this.clickCount ++;
+    this.log.push(new Date());
   }
 
 }
