2fc580e4f3ccf8950212de6737fbfdf54f58ba29
gem007bd <gem007bd@yahoo.com>
2019-07-10 12:46:00 +0200

    finish with level 6

--- src/app/components/recipes/recipes-list/recipes-list.component.ts
+++ src/app/components/recipes/recipes-list/recipes-list.component.ts
@@ -6,7 +6,7 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
-  @Output() recepeWasSelected = new EventEmitter<Recipe>();
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe('A test', 'simple description', 'http://sydneytimetocater.com.au/wp-content/uploads/2018/11/Meat-lamb-skewers.jpg'),
     new Recipe('A test 2', 'simple description 2', 'https://www.wallpaperup.com/uploads/wallpapers/2018/02/01/1199262/2c93886f34089d850ab0c838571d048b.jpg')
@@ -17,6 +17,6 @@ export class RecipesListComponent implements OnInit {
   ngOnInit() {
   }
   onRecipeSelected(recipe: Recipe) {
-    this.recepeWasSelected.emit(recipe);
+    this.recipeWasSelected.emit(recipe);
   }
 }
